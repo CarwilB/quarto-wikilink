7e8cad7c0e8cc9f6e552416a01607edd3a3af010
CarwilB <54286746+CarwilB@users.noreply.github.com>
2026-08-29 22:29:05 -0500

    Rewrite lua

--- _extensions/wikilink/wikilink.lua
+++ _extensions/wikilink/wikilink.lua
@@ -1,47 +1,77 @@
-local function file_exists(path)
-  local f = io.open(path, "rb")
-  if f then f:close() return true end
-  return false
-end
-
--- Read and encode bundled icon once during compilation
-local icon_path = quarto.utils.resolve_path("wikipedia-icon.png") or "wikipedia-icon.png"
-local icon_data_uri = ""
-
-if file_exists(icon_path) then
-  local f = io.open(icon_path, "rb")
-  local binary_data = f:read("*all")
-  f:close()
-  icon_data_uri = "data:image/png;base64," .. quarto.base64.encode(binary_data)
-end
+-- 1. Helper variable to hold our image data so we only load it once.
+local icon_data_uri = nil
 
 return {
   ['wikilink'] = function(args, kwargs)
-    local title = pandoc.utils.stringify(args[1] or "")
-    if title == "" then return pandoc.Null() end
+    -- 2. Ensure we have an argument for the Wikipedia page title
+    if not args[1] then
+      return pandoc.Null()
+    end
+    
+    local title = pandoc.utils.stringify(args[1])
+    if title == "" then
+      return pandoc.Null()
+    end
 
-    -- Custom display label or default to title
-    local label = args[2] and pandoc.utils.stringify(args[2]) or nil
+    -- 3. Check for an optional display label (the second argument)
+    local label = nil
+    if args[2] then
+      label = pandoc.utils.stringify(args[2])
+    end
 
-    -- Wikipedia slug conversion
+    -- 4. Convert the title to a valid Wikipedia URL (replace spaces with underscores)
     local page_slug = title:gsub(" ", "_")
     local url = "https://en.wikipedia.org/wiki/" .. page_slug
 
-    -- Icon styling
-    local img = pandoc.Image("Wikipedia", icon_data_uri)
-    img.attributes = {
-      width = kwargs["size"] or "1em",
-      style = "vertical-align: -0.15em; display: inline-block; border: none;"
-    }
+    -- 5. Load and base64 encode the icon lazily (only runs the first time the shortcode is used)
+    if not icon_data_uri then
+      -- PANDOC_SCRIPT_FILE is a built-in variable containing the exact path to this script
+      local script_path = PANDOC_SCRIPT_FILE or ""
+      -- Extract the directory path from the script path
+      local dir = script_path:match("(.*[/\\])") or "./"
+      local icon_path = dir .. "wikipedia-icon.png"
 
-    local link_elements = { img }
-
-    -- Append optional text label
-    if label then
-      table.insert(link_elements, pandoc.Space())
-      table.insert(link_elements, pandoc.Str(label))
+      local f = io.open(icon_path, "rb")
+      if f then
+        local binary_data = f:read("*all")
+        f:close()
+        -- Encode the binary data into a Data URI so it embeds directly into the HTML
+        icon_data_uri = "data:image/png;base64," .. quarto.base64.encode(binary_data)
+      else
+        icon_data_uri = "" -- Fallback if file is missing
+      end
     end
 
-    return pandoc.Link(link_elements, url, title)
+    -- 6. Construct the image attributes (handling the optional 'size' parameter)
+    local size = "1em"
+    if kwargs and kwargs["size"] then
+      size = pandoc.utils.stringify(kwargs["size"])
+    end
+    
+    local attr = pandoc.Attr(
+      "", -- identifier
+      {}, -- classes
+      {   -- key-value HTML attributes
+        width = size,
+        style = "vertical-align: -0.15em; display: inline-block; border: none;"
+      }
+    )
+
+    -- 7. Create the Pandoc Image element
+    -- Arguments: caption inlines, source URL, title string, attributes
+    local caption = { pandoc.Str("Wikipedia icon") }
+    local img = pandoc.Image(caption, icon_data_uri, "Wikipedia", attr)
+
+    -- 8. Build the content of the link
+    local link_content = { img }
+    
+    -- If a label was provided, add a space and the label text after the icon
+    if label and label ~= "" then
+      table.insert(link_content, pandoc.Space())
+      table.insert(link_content, pandoc.Str(label))
+    end
+
+    -- 9. Return the complete hyperlink wrapping our content
+    return pandoc.Link(link_content, url, title)
   end
 }
